a8f2a688e71d87d515f2a19cbfe319b3bf38eb53
Bhavleen Singh <singhbhavleen3@gmail.com>
2020-12-11 02:33:22 +0530

    Files Modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Details from "./components/Details";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <div className="d-flex">
+        <div>
+          <Sidebar />
+        </div>
+        <div className="white container shadow-lg">
+          <Details />
+        </div>
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default App;
